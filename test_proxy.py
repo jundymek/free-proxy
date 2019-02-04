791008a1751fc715c8ac597b4dc1199c2d9beb42
jundymek <jundymek@gmail.com>
2019-02-04 10:01:24 +0100

    Fixed tests.

--- test_proxy.py
+++ test_proxy.py
@@ -1,6 +1,4 @@
 import unittest
-import io
-import sys
 from unittest.mock import MagicMock
 
 from free_proxy import FreeProxy
@@ -9,14 +7,11 @@ from free_proxy import FreeProxy
 class TestProxy(unittest.TestCase):
 
     def test_empty_proxy_list(self):
-        # captured_output = io.StringIO()  # Create StringIO object
-        # sys.stdout = captured_output
-        thing = FreeProxy()
-        thing.get_proxy_list = MagicMock(return_value=[])
-        thing.get()
-        self.assertEqual(None, thing.get())
+        test = FreeProxy()
+        test.get_proxy_list = MagicMock(return_value=[])
+        self.assertEqual(None, test.get())
 
     def test_invalid_proxy(self):
-        thing = FreeProxy()
-        thing.get_proxy_list = MagicMock(return_value=['111.111.11:2222'])
-        self.assertEqual(None, thing.get())
+        test = FreeProxy()
+        test.get_proxy_list = MagicMock(return_value=['111.111.11:2222'])
+        self.assertEqual(None, test.get())
