08efca313fd1d393051776784dd7e8d925473cb8
la55u <andras9405@gmail.com>
2020-08-08 20:56:19 +0200

    added anonym filter

--- fp/fp.py
+++ fp/fp.py
@@ -9,10 +9,11 @@ import requests
 
 class FreeProxy:
 
-    def __init__(self, country_id=[], timeout=0.5, rand=False):
+    def __init__(self, country_id=[], timeout=0.5, rand=False, anonym=False):
         self.country_id = country_id
         self.timeout = timeout
         self.random = rand
+        self.anonym = anonym
 
     def get_proxy_list(self):
         try:
@@ -21,11 +22,13 @@ class FreeProxy:
             tr_elements = doc.xpath('//*[@id="proxylisttable"]//tr')
             if not self.country_id:
                 proxies = [f'{tr_elements[i][0].text_content()}:{tr_elements[i][1].text_content()}' for i in
-                           range(1, 101)]
+                           range(1, 101)
+                           if( (tr_elements[i][4].text_content()) == 'anonymous' if self.anonym else True) ] # check the 5th column for `anonymous` if needed
             else:
                 proxies = [f'{tr_elements[i][0].text_content()}:{tr_elements[i][1].text_content()}' for i in
                            range(1, 101)
-                           if tr_elements[i][2].text_content() in self.country_id]
+                           if tr_elements[i][2].text_content() in self.country_id
+                           and ( (tr_elements[i][4].text_content()) == 'anonymous' if self.anonym else True )] # check the 5th column for `anonymous` if needed
             return proxies
         except requests.exceptions.RequestException as e:
             print(e)
@@ -61,4 +64,3 @@ class FreeProxy:
             if r.raw.connection.sock:
                 if r.raw.connection.sock.getpeername()[0] == proxies['http'].split(':')[1][2:]:
                     return proxies['http']
-
--- test_proxy.py
+++ test_proxy.py
@@ -16,6 +16,12 @@ class TestProxy(unittest.TestCase):
         test.get_proxy_list = MagicMock(return_value=['111.111.11:2222'])
         self.assertEqual("There are no working proxies at this time.", test.get())
 
+    def test_anonym_filter(self):
+        test1 = FreeProxy()
+        cnt1 = len(test1.get_proxy_list())
+        test2 = FreeProxy(anonym=True)
+        cnt2 = len(test2.get_proxy_list())
+        self.assertTrue(cnt2 < cnt1)
 
 if __name__ == '__main__':
     unittest.main()
