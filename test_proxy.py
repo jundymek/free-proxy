5b3a083860ea16f5aa933e9fa66f601203206b7b
Lars Kue <lars@kuehmichel.de>
2020-09-06 15:32:04 +0200

    Modified Tests to accustom changes

--- test_proxy.py
+++ test_proxy.py
@@ -9,14 +9,14 @@ class TestProxy(unittest.TestCase):
     def test_empty_proxy_list(self):
         test = FreeProxy()
         test.get_proxy_list = MagicMock(return_value=[])
-        self.assertEqual(
-            "There are no working proxies at this time.", test.get())
+        with self.assertRaises(RuntimeError):
+            test.get()
 
     def test_invalid_proxy(self):
         test = FreeProxy()
         test.get_proxy_list = MagicMock(return_value=['111.111.11:2222'])
-        self.assertEqual(
-            "There are no working proxies at this time.", test.get())
+        with self.assertRaises(RuntimeError):
+            test.get()
 
     def test_anonym_filter(self):
         test1 = FreeProxy()
